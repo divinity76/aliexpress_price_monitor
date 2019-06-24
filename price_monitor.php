32cdd1709615093cbcf3bb36556d9d670a15ab61
divinity76 <divinity76@gmail.com>
2019-06-24 21:18:53 +0200

    captcha stuff INCOMPLETE G2G
    see https://github.com/divinity76/aliexpress_price_monitor/issues/1
    
    but i don't have time to investigate now, g2g

--- price_monitor.php
+++ price_monitor.php
@@ -4,6 +4,69 @@ require_once('hhb_.inc.php');
 global $cfg;
 init();
 $hc = new hhb_curl('', true);
+/**
+ * break captcha
+ *
+ * @param string $html
+ * @return boolean true if captcha broken, false if captcha not present.
+ */
+function break_captcha(string $html): bool
+{
+    if (false === strpos($html, 'document.getElementById("nocaptcha").style.cssText')) {
+        // no captcha. :)
+        return false;
+    }
+
+    // sheet, captcha =/ 
+    global $hc;
+    $domd = @DOMDocument::loadHTML($html);
+    $xp = new DOMXPath($xp);
+    //XA = <input name="nc_app_key" id="nc_app_key" type="hidden" value="X82Y__320d47f8fd1e72b46bce39afbc7e787e">
+    $xa = $nc_app_key = $xp->query("//input[@id='nc_app_key']")->item(0)->getAttribute("value");
+    // XT= <input name="nc_token" value="5633ba857fac8ade29b7a0e7579f410e" type="hidden">
+    $xt = $nc_token = $xp->query("//input[@name='nc_token']")->item(0)->getAttribute("value");
+    /*
+    getNcSession: function(e) {
+        return parseInt(e.offsetWidth + "a" + e.offsetHeight + "a" + this.getElementLeft(e) + "a" + this.getElementTop(e), 11).toString(16)
+    },
+    */
+    $url = 'https://cf.aliyun.com/nocaptcha/analyze.jsonp?' . http_build_query(
+        array(
+            'a' => $nc_app_key,
+            't' => $nc_token,
+            // ???? lets hope a replay attack is successful..
+            'n' => '118#ZVWZzXwWpbU8yewxOZ2CUeZTZeDh2g5NZZVTXsihzGSY2ZYVcoquZfqZZeZh0HZZZgxegOE8J4QVZZCu0oq4ze2ZZeOT0ip31cHuZYTTZeFzZgZ/VGTetHgzZZw6XHRuampYFoihYHbzZZZuVfq4zH2ZZZCh0HW4Zg2uZYqTihezZZZuVoi4+g2zFZZhV5d3qexgSxNh6XIBZoAZIY2gsm5JVaHQTj1WzT0tqJKvKwg7ZZe3L4PXZZxqrggmtBPUZYiP+7stpDS8GwQWjag29wS6r5PKIaX8EIGQ6zKpttLRigVhcpbdc1kOEmjaXwPZRLZ2Ea0YyfVq6sD4tssTm5yd7CB4fplH11weaeQoJiI4nFpbc4b7xKpuFhu8uKpjG4rcvIrddIC6zbXikVgmKdi9xhANUCseI9IWBFXj7nrPu3YWXmrG/9Pzvz3rddLi9UJXY67OVI5k3YtjwwAgUOR+0t3zc8iBIZRArjVpnLc4rwA0im4AqsLejkxSGbClQx59yHi80X2M63M+TDbYOTBuoNvcvRSxUX4jaZYfN8+md/KnDs8KKIXEze8zm2EioHJcUJMzDmH/aLa+Y3pgQNecmqsoqqKXW+eqmqxCzBmLW0KDJnOFdx5Obp1XGDAaBC53QCqgEHe6vFh8Ldgi6c/F2kW/1BjnY184irXmdHlUf+1Vg2PYcwh/q+0/yZNwuPRDB6zqI3eohRfWM5qUTYftkkVFuu52qUhCubuV+EXjCxR1aPhsru5vhgQjnbbpCFWTzXRehmEG7L8Hf4aKs+1V8vhOE/Pmq32OOndQLyhSFMovtdVctT1tdmKdDE6MS5Vg0jbd8Ckqk0gkrBwEJF2a12BdVH0FhYMv0J6ffGYQXnQkOR6Q1900r37Ysbl7FJnRgu8a+aA/Ljv+SOzEl34EkKS2r273LdAivwqv27bkrGLGw6pN4NCSx4tCHRQ2oH==',
+            // ??? try a replay attack.. found relevant javascript function tho
+            'p' => json_encode(array('ncSessionID' => '40ed14dea6ce5')),
+            'scene' => 'register',
+            'asyn' => '0',
+            'lang' => 'en',
+            // i think this is always 966, but the number is recieved from https://g.alicdn.com/sd/ncpc/nc.js 
+            'v' => '966',
+            // callback number is random.. should use rand() but i cba rn
+            'callback' => 'jsonp_06521602461982522',
+        )
+    );
+
+
+
+    $hc->setopt_array(array(
+        CURLOPT_URL => 'https://ynuf.aliapp.org/service/um.json',
+        CURLOPT_POST => 1,
+        CURLOPT_POSTFIELDS => http_build_query(array(
+            'data' => '106!7+Imc0clzvWHc0opjm+hz8WU43s8lLT3Vt0+f8UUzEH0nc6FU2umzIbjMJai5xmcnlOOKAvT+DeqpS6vxl580pRhP6g4inXBcbVEQs5berjzcsuCks9GAnvsxULu0K/gL0hybfKhBuBtLQV0qx3DAWM64wf5ow2ErGWsJZhQkCh1URFsssXJOrYH4vzQP7EggWmFyK3LBH5s4BwSsyXADHjcVZEEBT/Q4uDj6T/QsPAU5uKcsQoxDY1U9Mis/ysKmKUjxy/Qf0EeZ4cXxTpFTyddmIWFMfIXC0AaWysulE8SMeYe1jhgMae3zOxCFWXRaptTcbIOcb0ERhpwrmH/L2hGM24f7PTGTJy28b4FnbN75bzyUURdu3l7+lhuj2syqnxyDW8GzZONGIKBY4321258MRQwDXozdsZShL8fGL9uBc0UnFLr9qqGJ+QDB6U3gLXuYcBY4KbDOISIxbgJYaBUAzEsZsdtQcyncbMOXn0rfnEzCWnYZjbBdU0R54Mo368AcCJvBY2C5QBjHeEpt39ozM7tHeSTfNQ3griJLTjpGWZMePNjI0KMAgtT6/O+4pyZL3I1ep6BDn/t7w5Vc+kkv3JXQTICO3vErwRH0qS/5z8ogXPJROuegKDPV+lfNe88nF0WnqcavuTjuQJjDZUBkNLmSeYGKBUTKynTK/VEiovsdSRvvUcmPu0Sv1u1qGW=',
+            'xa' => $xa,
+            'xt' => $xt,
+            'efy' => 1
+        ))
+    ))->exec()->getStdOut();
+    /*
+xa	X82Y__320d47f8fd1e72b46bce39afbc7e787e
+xt	653550f81ee772cf56097b1f444240e1
+efy	1
+    */
+}
+
 while (true) {
     $full_loop_start = microtime(true);
     //$found_something = false;
@@ -63,7 +126,7 @@ while (true) {
             $first = null;
             // array_first()....
             foreach ($data['history'] as $record) {
-                if(!empty($record['unchanged'])){
+                if (!empty($record['unchanged'])) {
                     continue;
                 }
                 $first = $record;
@@ -83,7 +146,7 @@ while (true) {
                 }
             }
             if (!$changed) {
-                array_unshift($data['history'], array('unchanged'=>true, 'date' => pretty_date()));
+                array_unshift($data['history'], array('unchanged' => true, 'date' => pretty_date()));
             } else {
                 array_unshift($data['history'], $new_data);
             }
@@ -137,7 +200,9 @@ function parse_ali_html(string $html): array
     //$xp=new DOMXPath($domd);
     // original version: '/\<script\>\s*window\.runParams\s*\=\s*(?<json>\{[\s\S]+?\})\;\s*var/'
     // cpu-optimized version (provided by digitok@irc.freenode.net/#regex ): '/<script>\s*window\.runParams\s*=\s*(?<json>{(?:[^}]+|}(?!;\s*var))*+});/'
-    if (false === preg_match('/\<script\>\s*window\.runParams\s*=\s*(?<json>{(?:[^}]+|}(?!;\s*var))*+});/', $html, $matches)) {
+    if (!preg_match('/\<script\>\s*window\.runParams\s*=\s*(?<json>{(?:[^}]+|}(?!;\s*var))*+});/', $html, $matches)) {
+        // probably a captcha page...
+        hhb_var_dump($html);
         throw new \LogicException("regex failed to parse window.runParams from aliexpress html!");
     }
     $json = $matches['json'];
